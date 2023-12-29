e388b9ee4067c35da6306ea9236032bfde2f78fd
heony704 <heony704@users.noreply.github.com>
2023-12-29 11:32:34 +0000

    deployed via Gatsby Publish Action 🎩 for 7bc3e847ef597af163121d0a34530f023d78d27b

---
+++ component---src-pages-404-js-f3eb1ecbb1a8d3c342e4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkheony704_github_io=self.webpackChunkheony704_github_io||[]).push([[883],{45:function(e,t,n){n.r(t);var u=n(7294),l=n(1715),o=n(3432);t.default=function(){return u.createElement(l.Z,null,u.createElement(o.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f3eb1ecbb1a8d3c342e4.js.map
